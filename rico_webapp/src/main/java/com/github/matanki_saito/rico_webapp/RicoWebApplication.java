bcaf8610c5e27355b935d9ef685d15c596519eb2
matanki.saito <matanki.saito@gmail.com>
2021-02-19 14:40:21 +0900

    [app deploy]

--- rico_webapp/src/main/java/com/github/matanki_saito/rico_webapp/RicoWebApplication.java
+++ rico_webapp/src/main/java/com/github/matanki_saito/rico_webapp/RicoWebApplication.java
@@ -3,6 +3,9 @@ package com.github.matanki_saito.rico_webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * webapp
+ */
 @SpringBootApplication
 public class RicoWebApplication {
     public static void main(String[] args) {
