be6c5af49fd46d72ffbc0cbf626f8f72cb41194d
matanki.saito <matanki.saito@gmail.com>
2022-08-07 07:21:52 +0900

    [con build]

--- rico_console/src/main/resources/docker-entrypoint.sh
+++ rico_console/src/main/resources/docker-entrypoint.sh
@@ -9,6 +9,9 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+ls
+ls ./app/
+
 java -jar app/rico_console-all.jar -r="./"
 
 #java -jar app/rico_console-all.jar -r="${ROOT_DIR}" | reviewdog -efm="%f:%l:%c: %m" \
