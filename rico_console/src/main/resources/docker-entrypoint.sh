9545695c3adcad37e0fc5f4e6b5e2acf11b5b9dc
matanki.saito <matanki.saito@gmail.com>
2022-08-07 00:31:43 +0900

    test [con build]

--- rico_console/src/main/resources/docker-entrypoint.sh
+++ rico_console/src/main/resources/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+echo "start"
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
@@ -9,10 +9,14 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-java -jar app/rico_console-all.jar -r="${ROOT_DIR}" | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (rico)" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+java -jar app/rico_console-all.jar -r="./"
+
+#java -jar app/rico_console-all.jar -r="${ROOT_DIR}" | reviewdog -efm="%f:%l:%c: %m" \
+#      -name="linter-name (rico)" \
+#      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+#      -filter-mode="${INPUT_FILTER_MODE}" \
+#      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+#      -level="${INPUT_LEVEL}" \
+#      ${INPUT_REVIEWDOG_FLAGS}
+
+echo "success"
