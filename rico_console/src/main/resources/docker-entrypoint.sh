91c1c7d199209d18d40012fd1bb8587d11bc95e7
matanki.saito <matanki.saito@gmail.com>
2022-08-07 02:15:42 +0900

    test [con build]

--- rico_console/src/main/resources/docker-entrypoint.sh
+++ rico_console/src/main/resources/docker-entrypoint.sh
@@ -1,22 +1,5 @@
-#!/bin/sh
+#!/bin/sh -l
 
-echo "start"
-
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
-  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
-  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
-fi
-
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
-java -jar app/rico_console-all.jar -r="./"
-
-#java -jar app/rico_console-all.jar -r="${ROOT_DIR}" | reviewdog -efm="%f:%l:%c: %m" \
-#      -name="linter-name (rico)" \
-#      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-#      -filter-mode="${INPUT_FILTER_MODE}" \
-#      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-#      -level="${INPUT_LEVEL}" \
-#      ${INPUT_REVIEWDOG_FLAGS}
-
-echo "success"
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
